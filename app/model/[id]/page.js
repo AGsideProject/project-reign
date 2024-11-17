aa459e2ad79924bbd4be5adaf9832a372936df6d
gioiaswndr <annashkagioia24@gmail.com>
2024-11-17 23:29:12 +0800

    model detail page

--- app/model/[id]/page.js
+++ app/model/[id]/page.js
@@ -9,6 +9,7 @@ import 'swiper/css/navigation';
 
 //modules
 import { Pagination, Navigation } from 'swiper/modules'
+import Image from "next/image";
 
 export default function ModelDetail({ _params }) {
   // console.log(_params, "_params")
@@ -48,35 +49,105 @@ export default function ModelDetail({ _params }) {
   */
   return (
     <>
-      <div className="my-[30px] flex justify-center">
+      {/* <div className="my-[30px] flex justify-center">
         <div className="w-[85%] h-[65vh] bg-black">
           <img alt='model' src={url} className="w-full h-full object-cover" />
         </div>
+      </div> */}
+
+      {/* //! main carousel */}
+      <div className="my-[30px] flex justify-center">
+        <div className="w-[85vw] h-[65vh] bg-black">
+
+          <Swiper
+            // spaceBetween={0} //! gap between photo
+            slidesPerView={1}
+            // slidespreview={3}
+            navigation={false}
+            pagination={{ clickable: true }}
+            scrollbar={{ draggable: true }}
+            // className="mySwiper"
+            loop={true}
+            modules={[Pagination, Navigation]}
+            breakpoints={{
+              640: { // 640px is the default width for 'sm' in Tailwind CSS
+                slidesPerView: 2, // Show 2 slides on screens 640px or wider
+              },
+            }}
+          >
+            <SwiperSlide>
+              <div className="w-[100%] h-[65vh] bg-blue-400">
+                <img
+                  alt="mode"
+                  src={url}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[100%] h-[65vh] bg-blue-400">
+                <img
+                  alt="mode"
+                  src={url}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[100%] h-[65vh] bg-blue-400">
+                <img
+                  alt="mode"
+                  src={url}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[100%] h-[65vh] bg-blue-400">
+                <img
+                  alt="mode"
+                  src={url}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="w-[100%] h-[65vh] bg-blue-400">
+                <img
+                  alt="mode"
+                  src={url}
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
       </div>
+      {/* //! main carousel */}
 
       {/* //! fix phto */}
       <div className="bg-black flex justify-center items-center flex-wrap gap-2 py-2">
-        <div className="w-[100vw] h-[380px] mx-2 bg-blue-300">
+        <div className="w-[100vw] lg:w-[300px] md:w-[300px] h-[380px] mx-2 bg-blue-300">
           <img alt='model' src={url} className="w-full h-full object-cover" />
 
         </div>
-        <div className="w-[100vw] h-[380px] mx-2 bg-blue-300">
+        <div className="w-[100vw] lg:w-[300px] md:w-[300px] h-[380px] mx-2 bg-blue-300">
           <img alt='model' src={url} className="w-full h-full object-cover" />
 
         </div>
-        <div className="w-[100vw] h-[380px] mx-2 bg-blue-300">
+        <div className="w-[100vw] lg:w-[300px] md:w-[300px] h-[380px] mx-2 bg-blue-300">
           <img alt='model' src={url} className="w-full h-full object-cover" />
 
         </div>
-        <div className="w-[100vw] h-[380px] mx-2 bg-blue-300">
+        <div className="w-[100vw] lg:w-[300px] md:w-[300px] h-[380px] mx-2 bg-blue-300">
           <img alt='model' src={url} className="w-full h-full object-cover" />
 
         </div>
-        <div className="w-[100vw] h-[380px] mx-2 bg-blue-300">
+        <div className="w-[100vw] lg:w-[300px] md:w-[300px] h-[380px] mx-2 bg-blue-300">
           <img alt='model' src={url} className="w-full h-full object-cover" />
 
         </div>
-        <div className="w-[100vw] h-[380px] mx-2 bg-blue-300">
+        <div className="w-[100vw] lg:w-[300px] md:w-[300px] h-[380px] mx-2 bg-blue-300">
           <img alt='model' src={url} className="w-full h-full object-cover" />
 
         </div>
@@ -95,8 +166,9 @@ export default function ModelDetail({ _params }) {
       </div>
 
       <Swiper
-        spaceBetween={30}
-        slidespreview={5}
+        // spaceBetween={10}
+        slidesPerView={2}
+        // slidespreview={3}
         navigation={true}
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
@@ -104,12 +176,25 @@ export default function ModelDetail({ _params }) {
         loop={true}
         modules={[Pagination, Navigation]}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>
+          <Image alt="mode" src={url} width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image alt="mode" src={url} width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image alt="mode" src={url} width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image alt="mode" src={url} width={300} height={300} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image alt="mode" src={url} width={300} height={300} />
+        </SwiperSlide>
       </Swiper>
+
+
+
     </>
   )
 }
