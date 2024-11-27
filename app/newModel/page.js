bf34e46956b0c9a4f6f69a5da2a8fe462cf9d364
gioiaswndr <annashkagioia24@gmail.com>
2024-11-28 00:01:33 +0800

    akhirnya bisa jugaaaaaaa, update page model list

--- app/newModel/page.js
+++ app/newModel/page.js
@@ -6,59 +6,115 @@ import Header from "components/header";
 import { useEffect, useRef, useState } from "react";
 import { motion, useInView, useAnimation } from 'framer-motion'
 
-export default function page() {
-  const [data, setData] = useState([]);
+function Section({ children }) {
   const ref = useRef(null);
-
   const isInView = useInView(ref, { once: true });
 
-  const mainControls = useAnimation();
+  return (
+    <section ref={ref}>
+      <span
+        style={{
+          transform: isInView ? "none" : "translateX(-200px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+        }}
+      >
+        {children}
+      </span>
+    </section>
+  );
+}
 
-  useEffect(() => {
-    if (isInView) {
-      mainControls.start("visible");
-    }
-  }, [isInView])
+export default function page() {
+  // const [data, setData] = useState([]);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-      const jsonData = await response.json();
-      setData(jsonData);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
+  // const container = useRef(null)
+  // const ref = useRef(null)
+  // const isInView = useInView(ref, { amount: "all" })
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // // const isInView = useInView(ref)
+
+  // useEffect(() => {
+  //   console.log("Element is in view: ", isInView)
+  // }, [isInView])
+
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  //     const data_local = await response.json();
+  //     console.log(data_local, '<data json');
+  //     setData(data_local);
+  //   } catch (error) {
+  //     console.error('Error fetching data:', error);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
   return (
 
     <>
       <Header />
-      <motion.div
-        variants={{
-          hidden: { opacity: 0, y: 75 },
-          visible: { opacity: 1, y: 0 }
-        }}
-        initial="hidden"
-        animate={mainControls}
-        transition={{ duration: 0.5, delay: 0.25 }}
-      >
-        <section >
-          <div className="flex flex-wrap gap-5 items-center justify-center">
-            {
-              data.map((item) => (
-                <div ref={ref} key={item.id} className="bg-blue-200 w-[200px] h-[200px]">
-                  <h2>{item.title}</h2>
-                </div>
-              ))
-            }
-          </div>
-        </section>
-      </motion.div >
+
+      {/* <div ref={container} style={{ overflow: "scroll" }} className="flex flex-wrap gap-44 justify-center items-center my-40 px-6">
+
+        {
+          data.map((item, index) => (
+
+            <div ref={ref} key={index} className="bg-yellow-100 w-[50vw] h-[200px]">
+              {item.name}
+            </div>
+
+          ))
+        }
+
+        <div className="bg-yellow-100 w-[200px] h-[200px]">
+
+        </div>
+        <div className="bg-yellow-100 w-[200px] h-[200px]">
+
+        </div>
+        <div className="bg-yellow-100 w-[200px] h-[200px]">
+
+        </div>
+        <div className="bg-yellow-100 w-[200px] h-[200px]">
+
+        </div>
+        <div className="bg-yellow-100 w-[200px] h-[200px]">
+
+        </div>
+        <div className="bg-yellow-100 w-[200px] h-[200px]">
+
+        </div>
+        <div className="bg-yellow-100 w-[200px] h-[200px]">
+
+        </div>
+
+        <div className="bg-yellow-100 w-[200px] h-[200px]">
+
+        </div>
+        <div className="bg-yellow-100 w-[200px] h-[200px]">
+
+        </div>
+      </div> */}
+      <div className="w-[50vw] h-[500px] mx-20 bg-slate-400">
+        <Section>Animate</Section>
+      </div>
+      <div className="w-[50vw] h-[500px] mx-20 bg-slate-400">
+        <Section>when</Section>
+
+      </div>
+      <div className="w-[50vw] h-[500px] mx-20 bg-slate-400">
+        <Section>in</Section>
+
+      </div>
+      <div className="w-[50vw] h-[500px] mx-20 bg-slate-400">
+        <Section>view!</Section>
+
+      </div>
+
     </>
   );
 }
