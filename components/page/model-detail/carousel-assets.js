e969dab03f394ed35708ab110d5646fd3c29874c
gioiaswndr <annashkagioia24@gmail.com>
2025-02-14 00:35:37 +0800

    temporary carousel, fix grid

--- components/page/model-detail/carousel-assets.js
+++ components/page/model-detail/carousel-assets.js
@@ -12,7 +12,46 @@ const CarouselAssets = ({ data }) => {
 
   return (
     <div className="relative">
+
+      {/* //? CAROUSEL SEMENTARA */}
       <Swiper
+        slidesPerView="auto"
+        slidesPerGroup={2}
+        navigation
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        scrollbar={{ draggable: true }}
+        loop={false}
+        modules={[Navigation, Pagination]}
+        className="w-[85vw] sm:w-[55vw]"
+      >
+        {data.map((element, index) => (
+          <SwiperSlide
+            key={`carousel-assets-${index}`}
+            style={{
+              width: element.orientation === "portrait" ? "50%" : "100%",
+              //! yang ini aman tapi kepotong dikit yang 3/4
+              aspectRatio: element.orientation === "portrait" ? "3/4" : "16/9",
+              //! yang ini keliatan white blankspace tapi udh auto ngitung fotonya
+              // aspectRatio: "auto",
+            }}
+          >
+            <div className="flex justify-center items-center xl:h-[73vh]">
+              <img
+                src={element.img_url}
+                alt={`assets-model-${index}`}
+                className="object-cover w-full h-full"
+              />
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      {/* //? CAROUSEL SEMENTARA */}
+
+      {/* //!CAROUSEL ORIGINAL */}
+      {/* <Swiper
         slidesPerView="auto"
         slidesPerGroup={1}
         navigation
@@ -88,8 +127,10 @@ const CarouselAssets = ({ data }) => {
             </div>
           </SwiperSlide>
         ))}
-      </Swiper>
+      </Swiper> */}
+      {/* //!CAROUSEL ORIGINAL */}
 
+      {/* //! BUTTONNYA ( tinggal function do ) */}
       {/* <div className="absolute top-1/2 -left-1 transform -translate-y-1/2 -translate-x-full flex items-center justify-center text-white z-50 rounded-full">
 
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left" ><path d="m15 18-6-6 6-6" /></svg>
@@ -101,6 +142,7 @@ const CarouselAssets = ({ data }) => {
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right"><path d="m9 18 6-6-6-6" /></svg>
 
       </div> */}
+      {/* //! BUTTONNYA ( tinggal function do ) */}
 
     </div>
   );
