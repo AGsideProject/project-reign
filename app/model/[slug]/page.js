317aaa0acf9603397718cd1d11eb16df60d47319
aldomarcelino <aldo115marcelino@gmail.com>
2025-04-07 12:25:48 +0700

    fix detail

--- app/model/[slug]/page.js
+++ app/model/[slug]/page.js
@@ -1,36 +1,68 @@
+"use client";
+import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
-import fetchData from "services/fetch-ssr";
+import { useParams } from "next/navigation";
 import localFont from "next/font/local";
+import LoadingSpinner from "app/loading";
+import fetchData from "services/fetch-ssr";
 
 const resothoExtralight = localFont({
   src: "../../../public/fonts/ResothoExtralight-9YXJK.otf",
 });
 
-const butlerLightStencil = localFont({
-  src: "../../../public/fonts/Butler_Light_Stencil.otf",
-});
-
 const butlerMedium = localFont({
   src: "../../../public/fonts/Butler_Medium.otf",
-})
-
+});
 
 const ModelDetailComponent = dynamic(() =>
   import("components/page/model-detail")
 );
 
-const ModelDetailPage = async ({ params }) => {
-  const slug = (await params).slug;
-  const response = await fetchData(`/v1/model/${slug}`);
+const ModelDetailPage = () => {
+  const { slug } = useParams();
+  const [response, setResponse] = useState(null);
+  const [isNotFound, setIsNotFound] = useState(false);
+  const [, setError] = useState(null);
+
+  const getModelDetail = async () => {
+    try {
+      const data = await fetchData(`/v1/model/${slug}`);
+      console.log(data, "<<data");
+      setResponse(data);
+    } catch (error) {
+      const errMsg = error?.message || "";
+      setError(errMsg);
+      setIsNotFound(errMsg.includes("404"));
+    }
+  };
+
+  useEffect(() => {
+    getModelDetail();
+  }, []);
+
+  if (isNotFound)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <h5 className="text-xl font-semibold text-gray-500">Model not found</h5>
+      </div>
+    );
+
+  if (!response) return <LoadingSpinner />;
 
   return (
     <main>
-      <div className={`flex flex-col justify-center items-center mb-1 mt-14 ${resothoExtralight.className}`}>
+      <div
+        className={`flex flex-col justify-center items-center mb-1 mt-14 ${resothoExtralight.className}`}
+      >
         <div
           // className="mb-6 animate-fade-right animate-once animate-duration-[3000ms] animate-ease-in-out"
           className="mb-6"
         >
-          <p className={`text-4xl font-medium ${butlerMedium.className} tracking-[.7rem] uppercase`}>{response.name}</p>
+          <p
+            className={`text-4xl font-medium ${butlerMedium.className} tracking-[.7rem] uppercase`}
+          >
+            {response.name}
+          </p>
         </div>
 
         <div className="hidden sm:block">
@@ -40,7 +72,9 @@ const ModelDetailPage = async ({ params }) => {
               <p className="font-[100] text-slate-800">: {response.hight} CM</p>
             </div>
             <div className="flex">
-              <p className="font-[1000]">{response.gender === 'male' ? "CHEST ":'BUST '}</p>
+              <p className="font-[1000]">
+                {response.gender === "male" ? "CHEST " : "BUST "}
+              </p>
               <p className="font-[100] text-slate-800">: {response.bust} CM</p>
             </div>
             <div className="flex">
@@ -53,9 +87,11 @@ const ModelDetailPage = async ({ params }) => {
             </div>
             <div className="flex">
               <p className="font-[1000]">SHOE </p>
-              <p className="font-[100] text-slate-800">: {response.shoe_size} US</p>
+              <p className="font-[100] text-slate-800">
+                : {response.shoe_size} US
+              </p>
             </div>
-            <div className="flex" >
+            <div className="flex">
               <p className="font-[1000] ">HAIR </p>
               <p className="font-[100] text-slate-800">: {response.hair}</p>
             </div>
